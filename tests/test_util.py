c3b19b1506a63fe9189d37098438a81d00d7a43e
Andreas Motl <andreas.motl@panodata.org>
2025-03-14 11:49:32 +0100

    Core: Added streaming support for JSONL/NDJSON files

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,7 +6,7 @@ import jsonpointer
 import orjsonl
 import pytest
 import transon
-from tikray.util.data import load_json, save_json
+from tikray.util.data import load_json, save_json, to_list
 from tikray.util.expression import compile_expression
 from tikray.util.locator import to_pointer
 
@@ -54,15 +54,28 @@ def test_compile_expression_unknown():
     assert ex.match("Compilation failed. Type must be either jmes or jq or transon: foobar")
 
 
-def test_load_jsonl_by_suffix(tmp_path: Path):
-    data = load_json(Path("examples/eai-warehouse.json"))
+@pytest.mark.parametrize("input_", ["examples/acme/conversation.json", "examples/eai-warehouse.json"])
+def test_load_jsonl_by_suffix(tmp_path: Path, input_: str):
+    # Prepare.
+    data = load_json(Path(input_))
     tmp_path = tmp_path / "testdrive.jsonl"
-    orjsonl.save(tmp_path, [data])
-    assert load_json(tmp_path) == [data]
+    orjsonl.save(tmp_path, to_list(data))
+
+    # Validate.
+    assert list(load_json(tmp_path)) == to_list(data)
 
 
-def test_load_jsonl_by_flag(tmp_path: Path):
-    data = load_json(Path("examples/eai-warehouse.json"))
+@pytest.mark.parametrize("input_", ["examples/acme/conversation.json", "examples/eai-warehouse.json"])
+def test_load_jsonl_by_flag(tmp_path: Path, input_: str):
+    # Prepare.
+    data = load_json(Path(input_))
     tmp_path = tmp_path / "testdrive.json"
-    save_json([data], tmp_path, use_jsonl=True)
-    assert load_json(tmp_path, use_jsonl=True) == [data]
+    save_json(to_list(data), tmp_path, use_jsonl=True)
+
+    # Validate.
+    assert list(load_json(tmp_path, use_jsonl=True)) == to_list(data)
+
+
+def test_to_list():
+    assert to_list(None) is None
+    assert to_list("foobar") == ["foobar"]
