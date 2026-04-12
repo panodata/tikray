082052eb6772eac93d247c88e0bb5da26e0511e4
Andreas Motl <andreas.motl@panodata.org>
2026-04-12 14:39:42 +0200

    MacroPipe: Simplify test cases, now using LazyFrame across the board

--- tests/test_macropipe.py
+++ tests/test_macropipe.py
@@ -9,58 +9,46 @@ from tikray.macropipe import MacroPipe, recipe
 
 def test_head():
     """Validate the `head` function."""
-    input_frame = pl.DataFrame({"value": [42.42, 84.84]}).lazy()
-    output_frame = pl.DataFrame({"value": [42.42]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42, 84.84]})
+    output_frame = pl.LazyFrame({"value": [42.42]})
     pipe = MacroPipe.from_recipes(
         "head:1",
     )
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_tail():
     """Validate the `tail` function."""
-    input_frame = pl.DataFrame({"value": [42.42, 84.84]}).lazy()
-    output_frame = pl.DataFrame({"value": [84.84]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42, 84.84]})
+    output_frame = pl.LazyFrame({"value": [84.84]})
     pipe = MacroPipe.from_recipes(
         "tail:1",
     )
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_first():
     """Validate the `first` function."""
-    input_frame = pl.DataFrame({"value": [42.42, 84.84]}).lazy()
-    output_frame = pl.DataFrame({"value": [42.42]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42, 84.84]})
+    output_frame = pl.LazyFrame({"value": [42.42]})
     pipe = MacroPipe.from_recipes(
         "first",
     )
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_last():
     """Validate the `last` function."""
-    input_frame = pl.DataFrame({"value": [42.42, 84.84]}).lazy()
-    output_frame = pl.DataFrame({"value": [84.84]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42, 84.84]})
+    output_frame = pl.LazyFrame({"value": [84.84]})
     pipe = MacroPipe.from_recipes(
         "last",
     )
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_format():
@@ -69,46 +57,37 @@ def test_format():
 
     https://docs.pola.rs/api/python/stable/reference/expressions/api/polars.format.html
     """
-    input_frame = pl.DataFrame({"a": ["a", "b", "c"], "b": [1, 2, 3]}).lazy()
-    output_frame = pl.DataFrame({"value": ["foo_a_bar_1", "foo_b_bar_2", "foo_c_bar_3"]}).lazy()
+    input_frame = pl.LazyFrame({"a": ["a", "b", "c"], "b": [1, 2, 3]})
+    output_frame = pl.LazyFrame({"value": ["foo_a_bar_1", "foo_b_bar_2", "foo_c_bar_3"]})
     pipe = MacroPipe.from_recipes(
         "format:foo_{}_bar_{}:a,b:value:drop=true",
     )
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_apply_macropipe():
     """Validate MacroPipe's `apply` function."""
-    input_frame = pl.DataFrame({"value": [42.42]}).lazy()
-    output_frame = pl.DataFrame({"value": ["42.42"]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42]})
+    output_frame = pl.LazyFrame({"value": ["42.42"]})
     pipe = MacroPipe.from_recipes(
         "cast:value:str",
     )
     # Invoke `apply` on the `MacroPipe` instance.
     converted_frame = pipe.apply(input_frame)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_apply_extension():
     """Validate the extension's `apply` function."""
-    input_frame = pl.DataFrame({"value": [42.42]}).lazy()
-    output_frame = pl.DataFrame({"value": ["42.42"]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42]})
+    output_frame = pl.LazyFrame({"value": ["42.42"]})
     pipe = MacroPipe.from_recipes(
         "cast:value:str",
     )
     # Invoke `apply` on the `mp` namespace of the `LazyFrame` instance.
     converted_frame = input_frame.mp.apply(pipe)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_user_registered_recipe():
@@ -120,32 +99,26 @@ def test_user_registered_recipe():
     def hello(lf: pl.LazyFrame, column_name: str) -> pl.LazyFrame:
         return lf.with_columns(pl.concat_str(pl.lit("Hello"), pl.col(column_name), separator=" ").alias(column_name))
 
-    input_frame = pl.DataFrame({"value": [42.42]}).lazy()
-    output_frame = pl.DataFrame({"value": ["Hello 42.42"]}).lazy()
+    input_frame = pl.LazyFrame({"value": [42.42]})
+    output_frame = pl.LazyFrame({"value": ["Hello 42.42"]})
     pipe = MacroPipe.from_recipes(
         "hello:value",
     )
     converted_frame = pipe.apply(input_frame)
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_cast_scalar_str_macro():
     """
     Validate casting columns to string type, macro style.
     """
-    input_frame = pl.DataFrame({"float": [42.42], "int": [42], "str": ["42"]})
-    output_frame = pl.DataFrame({"float": ["42.42"], "int": ["42"], "str": ["42"]})
+    input_frame = pl.LazyFrame({"float": [42.42], "int": [42], "str": ["42"]})
+    output_frame = pl.LazyFrame({"float": ["42.42"], "int": ["42"], "str": ["42"]})
     pipe = MacroPipe.from_recipes(
         "cast:float,int,str:str",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_cast_scalar_str_extension():
@@ -155,26 +128,20 @@ def test_cast_scalar_str_extension():
     input_frame = pl.LazyFrame({"float": [42.42], "int": [42], "str": ["42"]})
     output_frame = pl.LazyFrame({"float": ["42.42"], "int": ["42"], "str": ["42"]})
     converted_frame = input_frame.mp.cast("float,int,str", "str")
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_cast_scalar_float():
     """
     Validate casting columns to float type.
     """
-    input_frame = pl.DataFrame({"float": ["42.42"], "int": ["42"], "str": ["42"]})
-    output_frame = pl.DataFrame({"float": [42.42], "int": [42.0], "str": [42.0]})
+    input_frame = pl.LazyFrame({"float": ["42.42"], "int": ["42"], "str": ["42"]})
+    output_frame = pl.LazyFrame({"float": [42.42], "int": [42.0], "str": [42.0]})
     pipe = MacroPipe.from_recipes(
         "cast:float,int,str:float",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_select_columns():
@@ -185,15 +152,13 @@ def test_select_columns():
 timestamp,data,garbage1,garbage2
 1754784000000,foo,bar,baz
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv))
+    output_frame = pl.LazyFrame({"timestamp": [1754784000000], "data": ["foo"]})
     pipe = MacroPipe.from_recipes(
         "select:timestamp,data",
     )
-    df = pl.scan_csv(StringIO(csv))
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"timestamp": [1754784000000], "data": ["foo"]}),
-    )
+    converted_frame = input_frame.mp.apply(pipe)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_drop_columns():
@@ -204,15 +169,13 @@ def test_drop_columns():
 timestamp,data,garbage1,garbage2
 1754784000000,foo,bar,baz
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv))
+    output_frame = pl.LazyFrame({"timestamp": [1754784000000], "data": ["foo"]})
     pipe = MacroPipe.from_recipes(
         "drop:garbage1,garbage2",
     )
-    df = pl.scan_csv(StringIO(csv))
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"timestamp": [1754784000000], "data": ["foo"]}),
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_rename_column():
@@ -223,79 +186,65 @@ def test_rename_column():
 _id,data
 42,Hotzenplotz
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv))
+    output_frame = pl.LazyFrame({"__id": [42], "data": ["Hotzenplotz"]})
     pipe = MacroPipe.from_recipes(
         "rename:_id:__id",
     )
-    df = pl.scan_csv(StringIO(csv))
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"__id": [42], "data": ["Hotzenplotz"]}),
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_concat():
     """
     Validate combining columns.
     """
-    input_frame = pl.DataFrame({"float": [42.42], "int": [42], "str": ["42"]})
-    output_frame = pl.DataFrame({"combined": ["42.42,42,42"]})
+    input_frame = pl.LazyFrame({"float": [42.42], "int": [42], "str": ["42"]})
+    output_frame = pl.LazyFrame({"combined": ["42.42,42,42"]})
     pipe = MacroPipe.from_recipes(
         "concat:float,int,str:,:combined:drop=true",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_scale():
     """
     Validate scaling values.
     """
-    input_frame = pl.DataFrame({"value": [4242]})
-    output_frame = pl.DataFrame({"value": [42.42]})
+    input_frame = pl.LazyFrame({"value": [4242]})
+    output_frame = pl.LazyFrame({"value": [42.42]})
     pipe = MacroPipe.from_recipes(
         "scale:value:100",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_iso_to_unixtime():
     """
     Validate converting ISO-8601 format to epoch timestamp (Unix time).
     """
-    input_frame = pl.DataFrame({"value": ["2026-03-03T12:12:12"]})
-    output_frame = pl.DataFrame({"value": [1772539932]})
+    input_frame = pl.LazyFrame({"value": ["2026-03-03T12:12:12"]})
+    output_frame = pl.LazyFrame({"value": [1772539932]})
     pipe = MacroPipe.from_recipes(
         "iso_to_unixtime:value",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_unixtime_to_iso():
     """
     Validate converting epoch timestamp (Unix time) to ISO-8601 format.
     """
-    input_frame = pl.DataFrame({"value": [1772539932]})
-    output_frame = pl.DataFrame({"value": ["2026-03-03T12:12:12.000000"]})
+    input_frame = pl.LazyFrame({"value": [1772539932]})
+    output_frame = pl.LazyFrame({"value": ["2026-03-03T12:12:12.000000"]})
     pipe = MacroPipe.from_recipes(
         "unixtime_to_iso:value",
     )
-    converted_frame = pipe.apply(input_frame.lazy()).collect()
-    assert_frame_equal(
-        converted_frame,
-        output_frame,
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_earth_observations_coords_python():
@@ -315,18 +264,16 @@ def test_earth_observations_coords_python():
 timestamp,coords,data
 1754784000000,"[9.757, 47.389]","{'temperature': 42.42}"
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv), quote_char='"')
+    output_frame = pl.LazyFrame(
+        {"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"], "data": ['{"temperature":42.42}']}
+    )
     pipe = MacroPipe.from_recipes(
         "json_array_to_wkt_point:coords",
         "python_to_json:data",
     )
-    df = pl.scan_csv(StringIO(csv), quote_char='"')
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame(
-            {"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"], "data": ['{"temperature":42.42}']}
-        ),
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_combine_coords_cells():
@@ -338,16 +285,14 @@ def test_combine_coords_cells():
 timestamp,longitude,latitude
 1754784000000,9.757,47.389
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv))
+    output_frame = pl.LazyFrame({"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"]})
     pipe = MacroPipe.from_recipes(
         "columns_to_json_array:longitude,latitude:coords:drop=true",
         "json_array_to_wkt_point:coords",
     )
-    df = pl.scan_csv(StringIO(csv))
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"]}),
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_extract_json_columns():
@@ -358,15 +303,13 @@ def test_extract_json_columns():
 timestamp,data
 1754784000000,'{"longitude": 9.757, "latitude": 47.389}'
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv), quote_char="'")
     pipe = MacroPipe.from_recipes(
         "json_fields_to_columns:data:longitude,latitude:drop=true",
     )
-    df = pl.scan_csv(StringIO(csv), quote_char="'")
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"timestamp": [1754784000000], "longitude": ["9.757"], "latitude": ["47.389"]}),
-    )
+    output_frame = pl.LazyFrame({"timestamp": [1754784000000], "longitude": ["9.757"], "latitude": ["47.389"]})
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
 
 
 def test_extract_json_coords():
@@ -378,12 +321,10 @@ def test_extract_json_coords():
 timestamp,data
 1754784000000,"{""lon"": 9.757, ""lat"": 47.389}"
     """.strip()
+    input_frame = pl.scan_csv(StringIO(csv))
+    output_frame = pl.LazyFrame({"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"]})
     pipe = MacroPipe.from_recipes(
         "json_fields_to_wkt_point:data:lon:lat:coords:drop=true",
     )
-    df = pl.scan_csv(StringIO(csv))
-    df = pipe.apply(df).collect()
-    assert_frame_equal(
-        df,
-        pl.DataFrame({"timestamp": [1754784000000], "coords": ["POINT (9.757 47.389)"]}),
-    )
+    converted_frame = pipe.apply(input_frame)
+    assert_frame_equal(converted_frame, output_frame)
